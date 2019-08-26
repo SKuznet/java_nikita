9397a5c133f5fce6b30dad33ee70a5ffb77c4a91
SuperFlanker93 <s-w-d@mail.ru>
2019-08-26 20:53:07 +0300

    9.43

---
+++ src/main/java/HW4/s1s2Word.java
@@ -0,0 +1,21 @@
+package HW4;
+//9.43
+import java.util.Scanner;
+
+public class s1s2Word {
+    public static void main(String[] args) {
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите слово: ");
+            String word1 = in.nextLine();
+
+            for (int i = 0; i < word1.length(); i += 2) {
+                String res1 = String.valueOf(word1.charAt(i));
+                System.out.print(res1);
+
+            }
+            System.out.println();
+
+        }
+    }
+}
